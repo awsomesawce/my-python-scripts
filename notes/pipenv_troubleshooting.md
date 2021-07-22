e709e5107cb572a4b662772bf5afd9e5ad4d2d25
Carl C <awsomesawce@outlook.com>
2021-07-22 07:30:45 -0400

    add pipenv_troubleshooting notes, add packages to Pipfile, add util/internet_request.py

---
+++ notes/pipenv_troubleshooting.md
@@ -0,0 +1,4 @@
+# PIPENV Troubleshooting
+
+When trying to install requests into my virtualenv, I got an error on Windows.
+I think the reason is that I didn't have my virtualenv activated.  Now it seems to be working just fine.
--- util/internet_request.py
+++ util/internet_request.py
@@ -1,6 +1,5 @@
 #!python
 # Network and internet requests
-# OriginalLocation: $OneDrive/snippets/python/examples/internet_request.py
 """An attempt to use python stdlib to request a file on the web
 using urllib.request.urlopen() and write the data to a file.
 
@@ -11,14 +10,16 @@ __author__ = "Carl C"
 __license__ = """MIT"""
 
 import os
-import sys
+#import sys
 #from pprint import pprint
 #import requests
 import urllib.request as req
 
 def dlfile(urlname=None):
     """
-    Download file from url.  Incomplete
+    Download file from url.
+
+    Example:
     
     """
     rq = req.urlopen(urlname)
@@ -31,18 +32,19 @@ def convrequestedfile(urlname=None):
     """Convert requested file back to string like object"""
     result = req.urlopen(urlname)
     if result.code == 200:
-        readed = result.read() # Find a better word than "readed"
+        readed = result.read() # TODO: Find a better word than "readed"
         decoded = readed.decode()
         return decoded
     else:
-        print("There was a problem.")
-        print(f"Return code is {result.code}")
+        raise BaseException(r"There was a problem with the request")
 
 def writetofile(filename, string_to_write):
     """Takes a string and writes to a filename
     TODO: Figure out why os.path.exists is not WORKING
+
+    Example:
     """
-    if os.path.exists(os.path.basename(filename)):
+    if os.path.exists(filename):
         return f"Error: {filename} exists already"
     else:
         fd = open(filename, "w")
