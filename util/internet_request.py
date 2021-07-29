9be27b8901c5012189671feb9bb5fb76a01ead33
Carl C <awsomesawce@outlook.com>
2021-07-29 00:44:01 -0400

    Add nice documentation to scripts, use pydoc to view it online or in terminal

--- util/internet_request.py
+++ util/internet_request.py
@@ -10,18 +10,24 @@ __author__ = "Carl C"
 __license__ = """MIT"""
 
 import os
-#import sys
-#from pprint import pprint
-#import requests
+
+# import sys
+# from pprint import pprint
+# import requests
 import urllib.request as req
+
 
 def dlfile(urlname=None):
     """
     Download file from url.
 
+    :param urlname: string of the url in question
+
+    :returns: a urlopen object
+
     Example:
     downloaded = dlfile("https://api.github.com")
-    
+
     """
     rq = req.urlopen(urlname)
     if rq.code == 200:
@@ -29,21 +35,22 @@ def dlfile(urlname=None):
     else:
         print("there was a problem")
 
+
 def convrequestedfile(urlname=None):
     """Convert requested file back to string like object
-    Example:
-    decodedrequest = convrequestedfile('https://httpbin.org/get')
+    :example: decodedrequest = convrequestedfile('https://httpbin.org/get')
 
-    Returns a string that is already decoded
-    See `writetofile` for a function to write the string to a file
+    :returns: a string that is already decoded
+    See ``writetofile`` for a function to write the string to a file
     """
     result = req.urlopen(urlname)
     if result.code == 200:
-        readed = result.read() # TODO: Find a better word than "readed"
+        readed = result.read()  # TODO: Find a better word than "readed"
         decoded = readed.decode()
         return decoded
     else:
         raise BaseException(r"There was a problem with the request")
+
 
 def writetofile(filename, string_to_write):
     """Takes a string and writes to a filename
@@ -54,12 +61,13 @@ def writetofile(filename, string_to_write):
     if os.path.exists(filename):
         return f"Error: {filename} exists already"
     else:
-        with open(filename, "w", encoding='utf8') as fd:
+        with open(filename, "w", encoding="utf8") as fd:
             fd.write(string_to_write)
             fd.close()
-            
+
+
 __example__ = """
-Check docstrings
+Check docstrings for examples
 """
 
-"Now I just need to find the correct stdlib module that allows writing to the filesystem."
+__comments__ = "Now I just need to find the correct stdlib module that allows writing to the filesystem."
--- util/usingMyModule.py
+++ util/usingMyModule.py
@@ -1,11 +1,25 @@
-"""Using internet_request.py"""
+"""
+====================
+Using Module Imports
+====================
+
+Using internet_request.py as a module import
+
+This is a demonstration of package resolution in Python.
+
+It seems to work pretty nicely, and python's integrated
+documentation support is just such a nice thing to have.  It is nice that
+it is built *into the language* instead of having to rely on some third-party tooling.
+
+
+"""
 import sys
 import os
 import getopt
 
-import internet_request
+import './internet_request.py' as ireq
 
-myreq = internet_request
+myreq = ireq
 
 myUrl = "https://httpbin.org"
 
