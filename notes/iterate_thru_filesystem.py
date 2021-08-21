b7c7a4b19fa748e2b7f97dfb6c50c99fd89ac340
Carl C <awsomesawce@outlook.com>
2021-08-21 17:15:48 -0400

    Add notes/.yapf_config.py         notes/iterate_thru_filesystem.py

---
+++ notes/iterate_thru_filesystem.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Using python to iterate thru file system objects
+
+import os
+import sys
+from pathlib import Path
+from pprint import pprint
+
+def ls(d=os.getcwd()):
+	d_abs = os.path.abspath(d)
+	if os.path.exists(d):
+		print(f"Here is the contents of {d_abs}:")
+		return pprint(os.listdir(d_abs))
+	else:
+		raise Exception("{d} path does not exist")
+
+def funWithPaths(path):
+    return Path(path)
+
+ls('..')
+
+file_path = funWithPaths("./Conda.md")
+
+##print(file_path)
+
+##def changeSysPath(p, options):
+##    return [p, options]
+##
+##print(changeSysPath("one", "two"))
